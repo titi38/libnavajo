f5a36d2ccdd012006caa27df08502f0372c27e3f
Thierry Descombes <thierry.descombes@lpsc.in2p3.fr>
2015-04-16 18:05:27 +0200

    deb fix

--- dpkgBuild.sh
+++ dpkgBuild.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-cd ..
-export DPKG_BUILD_ROOT=debian
+export DPKG_BUILD_ROOT=debianBuild
 mkdir -p $DPKG_BUILD_ROOT
 make install PREFIX=$DPKG_BUILD_ROOT/usr
 dpkg -b $DPKG_BUILD_ROOT libnavajo-1.0.amd64.deb
