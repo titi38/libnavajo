9261c9a1486fb86c972855a11ab093850b484648
descombes <titi38@github.com>
2017-04-07 22:39:38 +0200

    add logOutput colors

--- src/LogStdOutput.cc
+++ src/LogStdOutput.cc
@@ -14,6 +14,14 @@
 #include <stdio.h>
 #include "libnavajo/LogStdOutput.hh"
 
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
 
 
   /***********************************************************************/
@@ -24,18 +32,26 @@
   */
   void LogStdOutput::append(const NvjLogSeverity& l, const std::string& message, const std::string& details)
   {
+   
     switch (l)
     {
       case NVJ_DEBUG:
+        fprintf(stdout, CYN "%s\n" RESET, message.c_str()); fflush(NULL);
+        break;
       case NVJ_WARNING:
+        fprintf(stdout, YEL "%s\n" RESET, message.c_str()); fflush(NULL);
+        break;
       case NVJ_ALERT:
+        fprintf(stdout, MAG "%s\n" RESET, message.c_str()); fflush(NULL);
+        break;
       case NVJ_INFO:
-        fprintf(stdout,"%s\n",message.c_str()); fflush(NULL);
+        fprintf(stdout, GRN "%s\n" RESET, message.c_str()); fflush(NULL);
         break;
       case NVJ_ERROR:
+        fprintf(stderr, RED "%s\n" RESET, message.c_str());
+        break;
       case NVJ_FATAL:
-      default:
-        fprintf(stderr,"%s\n",message.c_str()); 
+        fprintf(stderr, RED "%s\n" RESET,message.c_str()); 
         break;
     }
   }
